f52bf2061685d0ca125c47dcf59b578b583ee651
dongig.shin <dongig.shin@samsung.com>
2025-07-02 16:05:52 +0900

    check1

--- SimilarityChecker/SimilarityChecker.cpp
+++ SimilarityChecker/SimilarityChecker.cpp
@@ -1,5 +1,36 @@
 #include "gmock/gmock.h"
 
 class SimilarityChecker {
+public:
+	int check(std::string str1, std::string str2) {
+		int ret = 0;
+		if (isSameString(str1, str2))
+			return 100;
 
+		if (isDoubleLength(str1, str2))
+			return 0;
+
+		if (str1.length() == str2.length()) {
+			ret += 60;
+		}
+
+		else {
+			int len1 = (str1.length() >= str2.length()) ? str1.length() : str2.length();
+			int len2 = (str1.length() <= str2.length()) ? str1.length() : str2.length();
+			ret += (1 - (len1 - len2)) / len2 * 60;
+		}
+
+		return ret;
+	}
+	bool isDoubleLength(std::string& str1, std::string& str2)
+	{
+		int length1 = str1.length();
+		int length2 = str2.length();
+		return (length1 > length2) ? 
+			length1 >= length2 * 2 : length1 * 2 >= length2;
+	}
+	bool isSameString(std::string& str1, std::string& str2)
+	{
+		return str1 == str2;
+	}
 };
