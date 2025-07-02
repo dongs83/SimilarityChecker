6c9ed3dd1eca9cd74c9bd0a3b2a9a297ed28abc2
dongig.shin <dongig.shin@samsung.com>
2025-07-02 15:24:00 +0900

    first commit

---
+++ SimilarityChecker/SimilarityChecker.cpp
@@ -0,0 +1,10 @@
+#include "gmock/gmock.h"
+
+class SimilarityChecker {
+
+};
+
+int main(){
+	testing::InitGoogleMock();
+	return RUN_ALL_TESTS();
+}
---
+++ SimilarityChecker/SimilarityChecker_test.cpp
@@ -0,0 +1,4 @@
+#include "gmock/gmock.h"
+TEST(SimilarityChecker, TC1) {
+	EXPECT_EQ(1, 1);
+}
